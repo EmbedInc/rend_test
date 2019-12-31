6daf7cc5e4827aea6766827960efae70ef04668d
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-12-31 13:57:03 -0500

    Added TEST_KEYS program.

--- build_progs.bat
+++ build_progs.bat
@@ -1,35 +1,36 @@
 @echo off
 rem
-rem   BUILD_PROGS [-dbg]
+rem   BUILD_PROGS
 rem
 rem   Build the executable programs from this source directory.
 rem
 setlocal
 call build_pasinit
 
-call src_prog %srcdir% test_2d %1
-call src_prog %srcdir% test_2dim %1
-call src_prog %srcdir% test_3d %1
-call src_prog %srcdir% test_3dt %1
-call src_prog %srcdir% test_aa %1
-call src_prog %srcdir% test_alpha %1
-call src_prog %srcdir% test_clip %1
-call src_prog %srcdir% test_quad %1
-call src_prog %srcdir% test_ray %1
-call src_prog %srcdir% test_shade %1
-call src_prog %srcdir% test_subpix %1
-call src_prog %srcdir% test_text %1
-call src_prog %srcdir% test_tmap %1
+call src_prog %srcdir% test_2d
+call src_prog %srcdir% test_2dim
+call src_prog %srcdir% test_3d
+call src_prog %srcdir% test_3dt
+call src_prog %srcdir% test_aa
+call src_prog %srcdir% test_alpha
+call src_prog %srcdir% test_clip
+call src_prog %srcdir% test_keys
+call src_prog %srcdir% test_quad
+call src_prog %srcdir% test_ray
+call src_prog %srcdir% test_shade
+call src_prog %srcdir% test_subpix
+call src_prog %srcdir% test_text
+call src_prog %srcdir% test_tmap
 
 goto :eof
 
 rem   These programs are not included for now because they have old Apollo OS
 rem   dependencies.
 rem
-rem call src_prog %srcdir% test_3dv %1
-rem call src_prog %srcdir% test_tube %1
-rem call src_prog %srcdir% test_tubeseg %1
+rem call src_prog %srcdir% test_3dv
+rem call src_prog %srcdir% test_tube
+rem call src_prog %srcdir% test_tubeseg
 
 rem   This program tries to cheat and get at the internals of RENDlib.
 rem
-rem call src_prog %srcdir% test_tmap2 %1
+rem call src_prog %srcdir% test_tmap2
