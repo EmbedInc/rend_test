aeacff1936fcf27fe759019cc145eece8a6500b7
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2024-06-01 11:18:50 -0400

    Updated to builds without circular dependencies.

--- build_pasinit.bat
+++ build_pasinit.bat
@@ -8,11 +8,7 @@ call src_get %srcdir% %libname%.ins.pas
 call src_get %srcdir% %libname%_all.ins.pas
 call src_get %srcdir% %libname%2.ins.pas
 
-call src_go %srcdir%
-call src_getfrom sys sys.ins.pas
-call src_getfrom util util.ins.pas
-call src_getfrom string string.ins.pas
-call src_getfrom file file.ins.pas
+call src_getbase
 call src_getfrom img img.ins.pas
 call src_getfrom vect vect.ins.pas
 call src_getfrom rend core rend.ins.pas
